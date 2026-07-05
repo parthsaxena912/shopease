d370f479ce62643995f0c1ded1b16f87d17a6f7c
parthsaxena912 <parthsaxena912@gmail.com>
2026-07-05 17:18:53 +0530

    Fix: proxy API calls through Nginx instead of hardcoded localhost

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5050/api';
+const client = axios.create({ baseURL: '/api' });
 
-const client = axios.create({ baseURL: API_BASE });
-
-// Attach the JWT token to every request if we have one
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
